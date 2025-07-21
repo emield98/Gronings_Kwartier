700cf4a35caa754069cf3a71d5fdc018468dcd86
emield98 <emieldost98@gmail.com>
2025-07-21 14:41:40 +0200

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata, Viewport } from 'next'
 import './globals.css'
+import { Analytics } from "@vercel/analytics/next"
+
 
 export const metadata: Metadata = {
   title: 'Gronings Kwartier',
@@ -37,7 +39,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="nl">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
